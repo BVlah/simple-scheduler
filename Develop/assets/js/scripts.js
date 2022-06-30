b6f395bdc10f94ded0ee5f7be205e22971467b39
Vlah <brennan.vlahcevic@armssoftware.com>
2022-06-30 00:05:55 -0400

    added color changing and save logo to buttons

--- Develop/assets/js/scripts.js
+++ Develop/assets/js/scripts.js
@@ -7,4 +7,27 @@ if (!events) [
   }
 ];
 
+
+// Display current day
 $("#currentDay").text(moment().format("dddd, MMMM Do YYYY"));
+
+// Compare time slot to current time and adjust color
+function setHourColor() {
+    var currentHour = moment().hours();
+
+    $(".description").each(function() {
+        var timeSlot = parseInt($(this).attr("id"));
+
+        if (timeSlot < currentHour) {
+            $(this).addClass("past");
+        }
+        else if (timeSlot > currentHour) {
+            $(this).addClass("future");
+        }
+        else {
+            $(this).addclass("present");
+        };
+    });
+};
+
+setHourColor();
